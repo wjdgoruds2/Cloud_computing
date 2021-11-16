bd2257cc5810c25ab7836956eca61e538b2d108e
wjdgoruds2 <wjdgoruds2@wjdgoruds2-15ZD970-GX50K>
2021-11-16 19:08:40 +0900

    list_instatnce

--- cloud_computing/src/ec2_instance.java
+++ cloud_computing/src/ec2_instance.java
@@ -4,12 +4,12 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
-import com.amazonaws.services.ec2.model.InstanceType;
-import com.amazonaws.services.ec2.model.RunInstancesRequest;
-import com.amazonaws.services.ec2.model.RunInstancesResult;
-import com.amazonaws.services.ec2.model.Tag;
+import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.Reservation;
 public class ec2_instance {
-	private AmazonEC2 ec2;
+	static AmazonEC2 ec2;
     private void init() throws Exception {
     	ProfileCredentialsProvider credentialsProvider = new ProfileCredentialsProvider();
 
@@ -24,7 +24,7 @@ public class ec2_instance {
 		}
 		ec2 = AmazonEC2ClientBuilder.standard()
 		.withCredentials(credentialsProvider)
-		.withRegion("us-east-1") 
+		.withRegion("us-east-2") 
 		.build();
     }
 
@@ -33,14 +33,21 @@ public class ec2_instance {
 		Scanner s = new Scanner(System.in);
 		boolean istrue=true;
 		while(istrue) {
-		    System.out.println("------------------------------------------------------------");
-		    System.out.println("	1. list instance		2. available zones"); 
-		    System.out.println("	3. start instance		4. available regions"); 
-		    System.out.println("	5. stop instance		6. create instance"); 
-		    System.out.println("	7. reboot instance 		8. list images"); 
-		    System.out.println("	99. quit"); 
-		    System.out.println("------------------------------------------------------------");
-		    System.out.print("Enter an integer:");
+			System.out.println(" ");
+			System.out.println(" ");
+			System.out.println("------------------------------------------------------------");
+			System.out.println(" Amazon AWS Control Panel using SDK ");
+			System.out.println(" ");
+			System.out.println(" Cloud Computing, Computer Science Department ");
+			System.out.println(" at Chungbuk National University ");
+			System.out.println("------------------------------------------------------------");
+			System.out.println(" 1. list instance 2. available zones ");
+			System.out.println(" 3. start instance 4. available regions ");
+			System.out.println(" 5. stop instance 6. create instance ");
+			System.out.println(" 7. reboot instance 8. list images ");
+			System.out.println(" 99. quit ");
+			System.out.println("------------------------------------------------------------");
+			System.out.print("Enter an integer: ");
 		    int num=s.nextInt();
 		    
 		    if (num==1) {
@@ -72,8 +79,36 @@ public class ec2_instance {
 		    }
 		  }
 	}
+	
 	public static void list_instance() {
-		System.out.println("1");
+		System.out.println("Listing instances....");
+		boolean done = false;
+		DescribeInstancesRequest request = new DescribeInstancesRequest();
+		while(!done) {
+			DescribeInstancesResult response = ec2.describeInstances(request);
+			
+			for(Reservation reservation : response.getReservations()) {
+					for(Instance instance : reservation.getInstances()) {
+						System.out.printf(
+						"[id] %s, " +
+						"[AMI] %s, " +
+						"[type] %s, " +
+						"[state] %10s, " +
+						"[monitoring state] %s",
+						instance.getInstanceId(),
+						instance.getImageId(),
+						instance.getInstanceType(),
+						instance.getState().getName(),
+						instance.getMonitoring().getState());
+						}
+					System.out.println();
+					}
+		request.setNextToken(response.getNextToken());
+		
+		if(response.getNextToken() == null) {
+			done = true;
+			}
+		}
 	}
 	public static void available_instance() {
 		System.out.println("2");
